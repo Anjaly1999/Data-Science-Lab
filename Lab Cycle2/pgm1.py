91c2b5f836e40c30cd0ecbde0a99e0bf66870827
Anjaly1999 <74658788+Anjaly1999@users.noreply.github.com>
2021-12-06 11:58:09 +0530

    Add files via upload

---
+++ Lab Cycle2/pgm1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+arr=np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]],dtype='f')
+print(arr)
