c931e7f193e0c3120cf25234f21075531d17c229
Anjaly1999 <74658788+Anjaly1999@users.noreply.github.com>
2021-12-06 11:19:39 +0530

    Add files via upload

---
+++ Lab Cycle2/pgm1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+a_3d_array = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+print(a_3d_array)
+
