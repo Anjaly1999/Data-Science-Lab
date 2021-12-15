0a83488fabc56df7415134a557409fbb3ff96b94
Anjaly1999 <74658788+Anjaly1999@users.noreply.github.com>
2021-12-15 11:30:59 +0530

    Add files via upload

---
+++ Lab Cycle 2 Part 2/pgm2.1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 15 11:17:20 2021
+
+@author: sjcet
+"""
+
+import numpy as np
+import numpy as nf
+from numpy.linalg import eig
+mat = np.random.randint(10, size=(3, 3))
+array = nf.random.randint(10, size=(3, 3))
+print(mat)
+M_inverse = np.linalg.inv(mat)
+print("inverse of the array")
+print(M_inverse)
+rank = np.linalg.matrix_rank(mat)
+print("Rank of the given Matrix ")
+print(rank)
+det= np.linalg.det(mat)
+print("determinant of the given Matrix ")
+print(det)
+arr=mat.flatten()
+print("transform matrix to array ")
+print(arr)
+w,v=eig(array)
+print('E-value:', w)
+print('E-vector', v)
