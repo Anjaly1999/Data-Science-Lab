0aa21b73d4095490fb093cddec931ca2a61ff5bf
Anjaly1999 <74658788+Anjaly1999@users.noreply.github.com>
2021-12-22 11:16:48 +0530

    Add files via upload

---
+++ Lab Cycle 3/c3_p4.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 20 12:08:26 2021
+
+@author: sjcet
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# create data
+x = [1,2,3,4,5]
+y = [3,3,3,3,3]
+z = [4,4,4,4,4]
+# plot lines
+plt.plot(x, y, label = "line 1")
+plt.plot(y, x, label = "line 2")
+plt.plot(z, x, label = "line 3")
+plt.legend()
+plt.show()
